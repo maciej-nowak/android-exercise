468592f468bc17f338c228f58650cc5b2d7ac833
Maciej Nowak <kontakt@maciej-nowak.pl>
2020-01-23 18:21:34 +0100

    feature/network - improve creation of fandom API service

--- app/src/main/java/pl/maciejnowak/exercise/interactor/TopArticlesInteractorImpl.kt
+++ app/src/main/java/pl/maciejnowak/exercise/interactor/TopArticlesInteractorImpl.kt
@@ -1,16 +1,14 @@
 package pl.maciejnowak.exercise.interactor
 
-import pl.maciejnowak.exercise.network.FandomApiService
+import pl.maciejnowak.exercise.network.Network
 import pl.maciejnowak.exercise.network.model.ExpandedArticleResultSet
 import java.io.IOException
 
 class TopArticlesInteractorImpl : TopArticlesInteractor {
 
-    private val service by lazy { FandomApiService.create() }
-
     override fun fetch(limit: Int): ExpandedArticleResultSet?{
         return try {
-            val response = service.getTopArticles(limit).execute()
+            val response = Network.fandomService.getTopArticles(limit).execute()
             if(response.isSuccessful) {
                 response.body()
             } else {
--- app/src/main/java/pl/maciejnowak/exercise/interactor/TopWikisInteractorImpl.kt
+++ app/src/main/java/pl/maciejnowak/exercise/interactor/TopWikisInteractorImpl.kt
@@ -1,16 +1,14 @@
 package pl.maciejnowak.exercise.interactor
 
-import pl.maciejnowak.exercise.network.FandomApiService
+import pl.maciejnowak.exercise.network.Network
 import pl.maciejnowak.exercise.network.model.ExpandedWikiaResultSet
 import java.io.IOException
 
 class TopWikisInteractorImpl : TopWikisInteractor {
 
-    private val service by lazy { FandomApiService.create() }
-
     override fun fetch(limit: Int): ExpandedWikiaResultSet? {
         return try {
-            val response = service.getTopWikis(limit).execute()
+            val response = Network.fandomService.getTopWikis(limit).execute()
             if(response.isSuccessful) {
                 response.body()
             } else {
