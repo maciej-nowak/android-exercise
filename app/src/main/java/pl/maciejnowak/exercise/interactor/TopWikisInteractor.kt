eabd8d5dcd828870c489de4fede401ba88425da6
Maciej Nowak <kontakt@maciej-nowak.pl>
2020-01-24 17:14:30 +0100

    feature/database - remove interactors

--- app/src/main/java/pl/maciejnowak/exercise/interactor/TopWikisInteractor.kt
+++
@@ -1,8 +0,0 @@
-package pl.maciejnowak.exercise.interactor
-
-import pl.maciejnowak.exercise.network.model.ExpandedWikiaResultSet
-
-interface TopWikisInteractor {
-
-    fun fetch(limit: Int = 30): ExpandedWikiaResultSet?
-}
