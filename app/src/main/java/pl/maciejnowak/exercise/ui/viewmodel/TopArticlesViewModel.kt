644a064f59f343a0eccfad3865f85007e8097edc
Maciej Nowak <kontakt@maciej-nowak.pl>
2020-02-06 18:37:16 +0100

    feature/improvements - handle loading state by flow

--- app/src/main/java/pl/maciejnowak/exercise/ui/viewmodel/TopArticlesViewModel.kt
+++ app/src/main/java/pl/maciejnowak/exercise/ui/viewmodel/TopArticlesViewModel.kt
@@ -2,6 +2,10 @@ package pl.maciejnowak.exercise.ui.viewmodel
 
 import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.withIndex
 import pl.maciejnowak.exercise.ui.repository.ArticleRepository
 import pl.maciejnowak.exercise.ui.viewmodel.model.TopArticlesResult
 
@@ -10,7 +14,11 @@ class TopArticlesViewModel(private val repository: ArticleRepository) : ViewMode
     private val retry: MutableLiveData<Boolean> = MutableLiveData(true)
 
     val result: LiveData<TopArticlesResult> = retry.switchMap {
-        repository.fetchTopArticlesFlow().asLiveData(Dispatchers.IO)
+        repository.fetchTopArticlesFlow().withIndex()
+            .onStart { _isLoading.postValue(true) }
+            .onEach { if(it.index == 0) { _isLoading.postValue(false) }}
+            .map { it.value }
+            .asLiveData(Dispatchers.IO)
     }
 
     private val _isLoading: MutableLiveData<Boolean> = MutableLiveData(true)
@@ -18,7 +26,6 @@ class TopArticlesViewModel(private val repository: ArticleRepository) : ViewMode
         get() = _isLoading
 
     fun loadTopArticles() {
-        _isLoading.value = true
         retry.value = true
     }
 }
--- app/src/main/java/pl/maciejnowak/exercise/ui/viewmodel/TopWikisViewModel.kt
+++ app/src/main/java/pl/maciejnowak/exercise/ui/viewmodel/TopWikisViewModel.kt
@@ -2,6 +2,10 @@ package pl.maciejnowak.exercise.ui.viewmodel
 
 import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.withIndex
 import pl.maciejnowak.exercise.ui.repository.WikiRepository
 import pl.maciejnowak.exercise.ui.viewmodel.model.TopWikisResult
 
@@ -10,7 +14,11 @@ class TopWikisViewModel(private val repository: WikiRepository) : ViewModel() {
     private val retry: MutableLiveData<Boolean> = MutableLiveData(true)
 
     val result: LiveData<TopWikisResult> = retry.switchMap {
-        repository.fetchTopWikisFlow().asLiveData(Dispatchers.IO)
+        repository.fetchTopWikisFlow().withIndex()
+            .onStart { _isLoading.postValue(true) }
+            .onEach { if(it.index == 0) { _isLoading.postValue(false) }}
+            .map { it.value }
+            .asLiveData(Dispatchers.IO)
     }
 
     private val _isLoading: MutableLiveData<Boolean> = MutableLiveData(true)
@@ -18,7 +26,6 @@ class TopWikisViewModel(private val repository: WikiRepository) : ViewModel() {
         get() = _isLoading
 
     fun loadTopWikis() {
-        _isLoading.value = true
         retry.value = true
     }
 }
