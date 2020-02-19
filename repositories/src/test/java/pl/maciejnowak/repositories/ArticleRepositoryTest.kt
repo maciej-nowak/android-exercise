57fed650c532706ec83259ac18e684482dee4be4
Maciej Nowak <kontakt@maciej-nowak.pl>
2020-02-19 18:07:08 +0100

    feature/testing - improve repositories tests

--- repositories/src/test/java/pl/maciejnowak/repositories/ArticleRepositoryTest.kt
+++ repositories/src/test/java/pl/maciejnowak/repositories/ArticleRepositoryTest.kt
@@ -1,7 +1,6 @@
 package pl.maciejnowak.repositories
 
 import kotlinx.coroutines.runBlocking
-import okhttp3.ResponseBody
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
@@ -24,12 +23,15 @@ import retrofit2.Response
 @RunWith(MockitoJUnitRunner::class)
 class ArticleRepositoryTest {
 
-    @Mock private lateinit var topArticle: TopArticle
-    @Mock private lateinit var topArticles: List<TopArticle>
     @Mock private lateinit var fandomService: FandomService
     @Mock private lateinit var articleDao: ArticleDao
     @Mock private lateinit var mapper: TopArticleMapper
     @InjectMocks private lateinit var repository: ArticleRepository
+
+    @Mock private lateinit var topArticle: TopArticle
+    @Mock private lateinit var topArticles: List<TopArticle>
+    @Mock private lateinit var responseSuccess: Response<ExpandedArticleResultSet>
+    @Mock private lateinit var responseError: Response<ExpandedArticleResultSet>
 
     @Before
     fun init() {
@@ -37,49 +39,52 @@ class ArticleRepositoryTest {
             `when`(articleDao.update(any())).thenReturn(Unit)
             `when`(articleDao.loadTopArticles()).thenReturn(topArticles)
         }
+        `when`(responseError.isSuccessful).thenReturn(false)
+        `when`(responseSuccess.isSuccessful).thenReturn(true)
+        `when`(responseSuccess.body()).thenReturn(mock(ExpandedArticleResultSet::class.java))
     }
 
     @Test
     fun localDataEmpty_fetchRemoteSuccess_resultSuccess() = runBlocking {
         `when`(articleDao.hasTopArticles()).thenReturn(null)
-        `when`(fandomService.getTopArticles(anyInt())).thenReturn(Response.success(mock(ExpandedArticleResultSet::class.java)))
-        assertEquals(TopArticlesResult.Success(topArticles), repository.fetchTopArticles(false))
+        `when`(fandomService.getTopArticles(anyInt())).thenReturn(responseSuccess)
+        assertEquals(TopArticlesResult.Success(topArticles), repository.fetchTopArticles())
     }
 
     @Test
     fun localDataEmpty_fetchRemoteError_resultError() = runBlocking {
         `when`(articleDao.hasTopArticles()).thenReturn(null)
-        `when`(fandomService.getTopArticles(anyInt())).thenReturn(Response.error(500, ResponseBody.create(null, "")))
-        assertEquals(TopArticlesResult.Error, repository.fetchTopArticles(false))
+        `when`(fandomService.getTopArticles(anyInt())).thenReturn(responseError)
+        assertEquals(TopArticlesResult.Error, repository.fetchTopArticles())
     }
 
     @Test
     fun localDataExpired_fetchRemoteSuccess_resultSuccess() = runBlocking {
         `when`(articleDao.hasTopArticles()).thenReturn(topArticle)
         `when`(articleDao.getTimeCreation()).thenReturn(0L)
-        `when`(fandomService.getTopArticles(anyInt())).thenReturn(Response.success(mock(ExpandedArticleResultSet::class.java)))
-        assertEquals(TopArticlesResult.Success(topArticles), repository.fetchTopArticles(false))
+        `when`(fandomService.getTopArticles(anyInt())).thenReturn(responseSuccess)
+        assertEquals(TopArticlesResult.Success(topArticles), repository.fetchTopArticles())
     }
 
     @Test
     fun localDataExpired_fetchRemoteError_resultErrorRefresh() = runBlocking {
         `when`(articleDao.hasTopArticles()).thenReturn(topArticle)
         `when`(articleDao.getTimeCreation()).thenReturn(0L)
-        `when`(fandomService.getTopArticles(anyInt())).thenReturn(Response.error(500, ResponseBody.create(null, "")))
-        assertEquals(TopArticlesResult.ErrorRefresh(topArticles), repository.fetchTopArticles(false))
+        `when`(fandomService.getTopArticles(anyInt())).thenReturn(responseError)
+        assertEquals(TopArticlesResult.ErrorRefresh(topArticles), repository.fetchTopArticles())
     }
 
     @Test
     fun localDataValid_forceRefresh_fetchRemoteSuccess_resultSuccess() = runBlocking {
         `when`(articleDao.hasTopArticles()).thenReturn(topArticle)
-        `when`(fandomService.getTopArticles(anyInt())).thenReturn(Response.success(mock(ExpandedArticleResultSet::class.java)))
+        `when`(fandomService.getTopArticles(anyInt())).thenReturn(responseSuccess)
         assertEquals(TopArticlesResult.Success(topArticles), repository.fetchTopArticles(true))
     }
 
     @Test
     fun localDataValid_forceRefresh_fetchRemoteError_resultErrorRefresh() = runBlocking {
         `when`(articleDao.hasTopArticles()).thenReturn(topArticle)
-        `when`(fandomService.getTopArticles(anyInt())).thenReturn(Response.error(500, ResponseBody.create(null, "")))
+        `when`(fandomService.getTopArticles(anyInt())).thenReturn(responseError)
         assertEquals(TopArticlesResult.ErrorRefresh(topArticles), repository.fetchTopArticles(true))
     }
 
@@ -87,8 +92,9 @@ class ArticleRepositoryTest {
     fun localDataValid_resultSuccess() = runBlocking {
         `when`(articleDao.hasTopArticles()).thenReturn(topArticle)
         `when`(articleDao.getTimeCreation()).thenReturn(System.currentTimeMillis())
-        assertEquals(TopArticlesResult.Success(topArticles), repository.fetchTopArticles(false))
+        assertEquals(TopArticlesResult.Success(topArticles), repository.fetchTopArticles())
     }
 
     private fun <T> any(): T = Mockito.any<T>()
+
 }
--- repositories/src/test/java/pl/maciejnowak/repositories/WikiRepositoryTest.kt
+++ repositories/src/test/java/pl/maciejnowak/repositories/WikiRepositoryTest.kt
@@ -1,7 +1,6 @@
 package pl.maciejnowak.repositories
 
 import kotlinx.coroutines.runBlocking
-import okhttp3.ResponseBody
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
@@ -24,12 +23,15 @@ import retrofit2.Response
 @RunWith(MockitoJUnitRunner::class)
 class WikiRepositoryTest {
 
-    @Mock private lateinit var topWiki: TopWiki
-    @Mock private lateinit var topWikis: List<TopWiki>
     @Mock private lateinit var fandomService: FandomService
     @Mock private lateinit var wikiDao: WikiDao
     @Mock private lateinit var mapper: TopWikiMapper
     @InjectMocks private lateinit var repository: WikiRepository
+
+    @Mock private lateinit var topWiki: TopWiki
+    @Mock private lateinit var topWikis: List<TopWiki>
+    @Mock private lateinit var responseSuccess: Response<ExpandedWikiaResultSet>
+    @Mock private lateinit var responseError: Response<ExpandedWikiaResultSet>
 
     @Before
     fun init() {
@@ -37,49 +39,52 @@ class WikiRepositoryTest {
             `when`(wikiDao.update(any())).thenReturn(Unit)
             `when`(wikiDao.loadTopWikis()).thenReturn(topWikis)
         }
+        `when`(responseError.isSuccessful).thenReturn(false)
+        `when`(responseSuccess.isSuccessful).thenReturn(true)
+        `when`(responseSuccess.body()).thenReturn(mock(ExpandedWikiaResultSet::class.java))
     }
 
     @Test
     fun localDataEmpty_fetchRemoteSuccess_resultSuccess() = runBlocking {
         `when`(wikiDao.hasTopWikis()).thenReturn(null)
-        `when`(fandomService.getTopWikis(anyInt())).thenReturn(Response.success(mock(ExpandedWikiaResultSet::class.java)))
-        assertEquals(TopWikisResult.Success(topWikis), repository.fetchTopWikis(false))
+        `when`(fandomService.getTopWikis(anyInt())).thenReturn(responseSuccess)
+        assertEquals(TopWikisResult.Success(topWikis), repository.fetchTopWikis())
     }
 
     @Test
     fun localDataEmpty_fetchRemoteError_resultError() = runBlocking {
         `when`(wikiDao.hasTopWikis()).thenReturn(null)
-        `when`(fandomService.getTopWikis(anyInt())).thenReturn(Response.error(500, ResponseBody.create(null, "")))
-        assertEquals(TopWikisResult.Error, repository.fetchTopWikis(false))
+        `when`(fandomService.getTopWikis(anyInt())).thenReturn(responseError)
+        assertEquals(TopWikisResult.Error, repository.fetchTopWikis())
     }
 
     @Test
     fun localDataExpired_fetchRemoteSuccess_resultSuccess() = runBlocking {
         `when`(wikiDao.hasTopWikis()).thenReturn(topWiki)
         `when`(wikiDao.getTimeCreation()).thenReturn(0L)
-        `when`(fandomService.getTopWikis(anyInt())).thenReturn(Response.success(mock(ExpandedWikiaResultSet::class.java)))
-        assertEquals(TopWikisResult.Success(topWikis), repository.fetchTopWikis(false))
+        `when`(fandomService.getTopWikis(anyInt())).thenReturn(responseSuccess)
+        assertEquals(TopWikisResult.Success(topWikis), repository.fetchTopWikis())
     }
 
     @Test
     fun localDataExpired_fetchRemoteError_resultErrorRefresh() = runBlocking {
         `when`(wikiDao.hasTopWikis()).thenReturn(topWiki)
         `when`(wikiDao.getTimeCreation()).thenReturn(0L)
-        `when`(fandomService.getTopWikis(anyInt())).thenReturn(Response.error(500, ResponseBody.create(null, "")))
-        assertEquals(TopWikisResult.ErrorRefresh(topWikis), repository.fetchTopWikis(false))
+        `when`(fandomService.getTopWikis(anyInt())).thenReturn(responseError)
+        assertEquals(TopWikisResult.ErrorRefresh(topWikis), repository.fetchTopWikis())
     }
 
     @Test
     fun localDataValid_forceRefresh_fetchRemoteSuccess_resultSuccess() = runBlocking {
         `when`(wikiDao.hasTopWikis()).thenReturn(topWiki)
-        `when`(fandomService.getTopWikis(anyInt())).thenReturn(Response.success(mock(ExpandedWikiaResultSet::class.java)))
+        `when`(fandomService.getTopWikis(anyInt())).thenReturn(responseSuccess)
         assertEquals(TopWikisResult.Success(topWikis), repository.fetchTopWikis(true))
     }
 
     @Test
     fun localDataValid_forceRefresh_fetchRemoteError_resultErrorRefresh() = runBlocking {
         `when`(wikiDao.hasTopWikis()).thenReturn(topWiki)
-        `when`(fandomService.getTopWikis(anyInt())).thenReturn(Response.error(500, ResponseBody.create(null, "")))
+        `when`(fandomService.getTopWikis(anyInt())).thenReturn(responseError)
         assertEquals(TopWikisResult.ErrorRefresh(topWikis), repository.fetchTopWikis(true))
     }
 
@@ -87,7 +92,7 @@ class WikiRepositoryTest {
     fun localDataValid_resultSuccess() = runBlocking {
         `when`(wikiDao.hasTopWikis()).thenReturn(topWiki)
         `when`(wikiDao.getTimeCreation()).thenReturn(System.currentTimeMillis())
-        assertEquals(TopWikisResult.Success(topWikis), repository.fetchTopWikis(false))
+        assertEquals(TopWikisResult.Success(topWikis), repository.fetchTopWikis())
     }
 
     private fun <T> any(): T = Mockito.any<T>()
